5ea3d3dbc5ad032976e512952cc496333d2e73d7
Misha Blinsky <99417065+MishaBlinsky@users.noreply.github.com>
2024-09-28 10:50:17 +0300

    now it says hello to user and asks them for their favourite language

--- lab1_Introduction_to_Ruby/ruby_basics.rb
+++ lab1_Introduction_to_Ruby/ruby_basics.rb
@@ -1,2 +1,20 @@
 # 1) output "hello world" with commentary
 puts "Hello WARUDO"
+
+# 2) Accept username as program's argument. Say hello to user using string formatting. Ask their favourite language with various answers.
+user_name = ARGV[0]
+puts "Hello, #{user_name}!"
+print "What's your favourite language? "
+language = STDIN.gets.chomp.downcase
+case language
+    when "ruby"
+        puts "Liar trying to appease me"
+    when "pascal"
+        puts "I see you're middle schooler"
+    when "c++"
+        puts "Unsurprising"
+    when "sharp"
+        puts "Soon it'll be Ruby"
+    else
+        puts "Nevermind"
+end
