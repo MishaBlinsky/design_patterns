19edcfdf0e4431c0db38cf7b54d4db634b98cf88
Misha Blinsky <99417065+MishaBlinsky@users.noreply.github.com>
2024-10-05 10:53:47 +0300

    Create methods.rb

---
+++ lab1_Introduction_to_Ruby/methods.rb
@@ -0,0 +1,7 @@
+# 4) write three methods (variant 4)
+
+# method 1 - find the amount of even numbers that are not relatively prime to a given number
+
+# method 2 - find maximal digits of a number that are not divided by 3
+
+# method 3 - [a lot of letters]
