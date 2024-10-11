7d5216b6b5ec7aad4a0ff7fd385ec5f66dc898f8
Misha Blinsky <99417065+MishaBlinsky@users.noreply.github.com>
2024-10-11 23:06:13 +0300

    Update methods.rb
    implemented the 1st method (description int the file)

--- lab1_Introduction_to_Ruby/methods.rb
+++ lab1_Introduction_to_Ruby/methods.rb
@@ -1,6 +1,22 @@
 # 4) write three methods (variant 4)
 
-# method 1 - find the amount of even numbers that are not relatively prime to a given number
+# method 1 - find the amount of even numbers that are not mutually prime to a given number
+def maxprimediv(a,b)
+    while b!=0
+        a,b = b,a%b
+    end
+    return a
+end
+def evnoncoprime_count(n)
+    count = 0
+    for i in 2..n
+        if i%2==0
+            count += 1 if maxprimediv(i,n)!=1
+        end
+    end
+    return count
+end
+print(evnoncoprime_count(21),"\n")
 
 # method 2 - find maximal digits of a number that are not divided by 3
 
