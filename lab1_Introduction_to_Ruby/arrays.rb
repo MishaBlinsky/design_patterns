a124ab980c4d6e3eadc73f9eb717c8a034f9becc
Misha Blinsky <99417065+MishaBlinsky@users.noreply.github.com>
2024-10-18 22:52:36 +0300

    Update arrays.rb
    Implemented array reader

--- lab1_Introduction_to_Ruby/arrays.rb
+++ lab1_Introduction_to_Ruby/arrays.rb
@@ -10,10 +10,33 @@ def find_min_elem (arr)
 end
 
 def find_1st_pos_elem (arr)
-    for i in 0..arr.lenght
+    return nil if arr.empty?
+    for i in 0..arr.length
         return i if arr[i] > 0
     end
     return -1
 end
 
 # 6) Write program that accepts 2 arguments. 1st one tells which method from task 5 to execute, 2nd tells path from where to read array.
+
+def main()
+    method_n = ARGV[0].to_i
+    file_path = ARGV[1].chomp
+    if (method_n == 1 || method_n == 2)
+        if File.exist?(file_path)
+            file = File.read(file_path)
+            arr = file.split(' ').map{|el| el.to_i}
+            case method_n
+                when 1
+                    puts "Min element: #{find_min_elem(arr)}"
+                when 2
+                    puts "1st pos element id: #{find_1st_pos_elem(arr)}"
+            end
+        else
+            puts "File not found!"
+        end
+    else
+        puts "Incorrect method number!"
+    end
+end
+main()
