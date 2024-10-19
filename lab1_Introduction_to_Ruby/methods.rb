9ebef8a87d727b749d872b8170c749aaf7b9a862
Misha Blinsky <99417065+MishaBlinsky@users.noreply.github.com>
2024-10-19 10:46:29 +0300

    Update methods.rb
    Reduced shitcodeness in 3rd method (+some corrections in previous ones)

--- lab1_Introduction_to_Ruby/methods.rb
+++ lab1_Introduction_to_Ruby/methods.rb
@@ -2,12 +2,12 @@
 
 # method 1 - find the amount of even numbers that are not mutually prime to a given number
 def max_prime_div(a,b)
-    while b! = 0
+    while b != 0
         a, b = b, a % b
     end
     return a
 end
-def evev_not_coprime_count(n)
+def even_not_coprime_count(n)
     count = 0
     for i in 2..n
         if i % 2 == 0
@@ -22,7 +22,7 @@ puts "#{even_not_coprime_count(21)}"
 
 def max_digit(a)
     b = -1
-    while a! = 0
+    while a != 0
         if (a % 10) % 3 != 0
             b = (a % 10).abs if (a % 10).abs > b   
         end
@@ -34,32 +34,24 @@ puts "#{max_digit(123)}"
 
 # method 3 - find a product of a max number, that are not mutually prime to a given number and not divided by a min divider of a given number, and a sum of digits of a given number that are lesser than 5
 
-def specprod(a)
-    mingivendiv = 2
-    while mingivendiv != a and a % mingivendiv != 0
-        mingivendiv += 1
+def spec_prod(a)
+    min_a_div = 2
+    while min_a_div != a and a % min_a_div != 0
+        min_a_div += 1
     end
-    maxnotprime = a
-    while maxnotprime != 0
-        if maxnotprime % mingivendiv != 0
-            break if max_prime_div(maxnotprime,a) != 1
+    max_not_coprime = a
+    while max_not_coprime != 0
+        if max_not_coprime % min_a_div != 0
+            break if max_prime_div(max_not_coprime, a) != 1
         end
-        maxnotprime -= 1
+        max_not_coprime -= 1
     end
-    if maxnotprime == 0
-        puts "oopsie - multiplication by zero"
-    else
-        digitsum = 0
-        while a != 0
-            digit = (a % 10).abs
-            digitsum += digit if digit < 5
-            a = (a/10).to_i
-        end
-        if digitsum == 0
-            puts "oopsie - multiplication by zero"
-        else
-            puts "#{maxnotprime} * #{digitsum} = #{maxnotprime * digitsum}"
-        end
+    digit_sum = 0
+    while a != 0
+        digit = (a % 10).abs
+        digit_sum += digit if digit < 5
+        a = (a/10).to_i
     end
+    return "#{max_not_coprime} * #{digit_sum} = #{max_not_coprime * digit_sum}"
 end
-specprod(123)
+puts "#{spec_prod(123)}"
