f09aa7329af17f17f3ce1add11809915d1be2ec2
Misha Blinsky <99417065+MishaBlinsky@users.noreply.github.com>
2024-10-19 10:36:50 +0300

    Update methods.rb
    Reduced shitcodeness in 1st method

--- lab1_Introduction_to_Ruby/methods.rb
+++ lab1_Introduction_to_Ruby/methods.rb
@@ -1,22 +1,22 @@
 # 4) write three methods (variant 4)
 
 # method 1 - find the amount of even numbers that are not mutually prime to a given number
-def maxprimediv(a,b)
-    while b!=0
-        a,b = b,a%b
+def max_prime_div(a,b)
+    while b! = 0
+        a, b = b, a % b
     end
     return a
 end
-def evnoncoprime_count(n)
+def evev_not_coprime_count(n)
     count = 0
     for i in 2..n
-        if i%2==0
-            count += 1 if maxprimediv(i,n)!=1
+        if i % 2 == 0
+            count += 1 if max_prime_div(i, n) != 1
         end
     end
     return count
 end
-puts "#{evnoncoprime_count(21)}"
+puts "#{even_not_coprime_count(21)}"
 
 # method 2 - find maximal digit of a number that are not divided by 3
 
@@ -46,7 +46,7 @@ def specprod(a)
     maxnotprime = a
     while maxnotprime != 0
         if maxnotprime % mingivendiv != 0
-            break if maxprimediv(maxnotprime,a) != 1
+            break if max_prime_div(maxnotprime,a) != 1
         end
         maxnotprime -= 1
     end
