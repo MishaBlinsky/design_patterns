bcd47add74065967acc96b84de3388f904cbb729
Misha Blinsky <99417065+MishaBlinsky@users.noreply.github.com>
2024-11-02 10:48:26 +0300

    Lab 2 Task 1.2 - added class objects

--- Lab2_Core_Classes_and_Models/main.rb
+++ Lab2_Core_Classes_and_Models/main.rb
@@ -1,1 +1,5 @@
-
+require_relative 'students'
+student1 = Student.new("Nikitich", "Nikita", "Nikitovich", 1, "88005553535", nil, nil, "nikit-git")
+student2 = Student.new("Zubenko", "Mihail", "Petrovich", 2, "12345678899", nil, nil, "mafia-git")
+puts student1
+puts student2
