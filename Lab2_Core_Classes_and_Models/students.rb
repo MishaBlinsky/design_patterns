bd9f7bbd3bf4864293dc222ccc71e0899c5c1759
Misha Blinsky <99417065+MishaBlinsky@users.noreply.github.com>
2024-11-02 10:26:33 +0300

    Lab 2 Task 1.1 - added student class with initialization method

--- Lab2_Core_Classes_and_Models/students.rb
+++ Lab2_Core_Classes_and_Models/students.rb
@@ -1,1 +1,60 @@
-
+class Student
+    def initialize(last_name, first_name, patronymic, id = nil, phone = nil, telegram = nil, email = nil, git = nil)
+        @last_name = last_name
+        @first_name = first_name
+        @patronymic = patronimyc
+        @id = id
+        @phone = phone
+        @telegram = telegram
+        @email = email
+        @git = git
+    end
+    def get_last_name
+        @last_name
+    end
+    def set_last_name (last_name)
+        @last_name = last_name
+    end
+    def get_first_name
+        @first_name
+    end
+    def set_first_name (first_name)
+        @first_name = first_name
+    end
+    def get_patronimyc
+        @patronimyc
+    end
+    def set_patronimyc (patronimyc)
+        @patronimyc = patronimyc
+    end
+    def get_id
+        @id
+    end
+    def set_id (id)
+        @id = id
+    end
+    def get_phone
+        @phone
+    end
+    def set_phone (phone)
+        @phone = phone
+    end
+    def get_telegram
+        @telegram
+    end
+    def set_telegram (telegram)
+        @telegram = telegram
+    end
+    def get_email
+        @email
+    end
+    def set_email (email)
+        @email = email
+    end
+    def get_git
+        @git
+    end
+    def set_git (git)
+        @git = git
+    end
+end
