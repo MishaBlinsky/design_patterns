db6346681b8021d9c1dbcfc16892a14bfc733606
Misha Blinsky <99417065+MishaBlinsky@users.noreply.github.com>
2024-11-09 10:26:19 +0300

    Lab 2 Task 1.5 - adjusted class objects according to class changes

--- Lab2_Core_Classes_and_Models/main.rb
+++ Lab2_Core_Classes_and_Models/main.rb
@@ -1,5 +1,5 @@
 require_relative 'students'
-student1 = Student.new("Nikitich", "Nikita", "Nikitovich", 1, "88005553535", nil, nil, "nikit-git")
-student2 = Student.new("Zubenko", "Mihail", "Petrovich", 2, "12345678899", nil, nil, "mafia-git")
+student1 = Student.new("Morales", "Miles", id: 1, phone: "88005553535", git: "abas-git")
+student2 = Student.new("Zubenko", "Mihail", patronymic: "Petrovich", id: 2, phone: "12345678899", git: "mafia-git")
 puts student1
 puts student2
