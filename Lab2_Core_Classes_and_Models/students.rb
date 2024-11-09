16a8798ab39d4850e8838884f704af949f23e6ab
Misha Blinsky <99417065+MishaBlinsky@users.noreply.github.com>
2024-11-09 10:46:30 +0300

    Lab 2 Task 1.7 - added regex to phone number

--- Lab2_Core_Classes_and_Models/students.rb
+++ Lab2_Core_Classes_and_Models/students.rb
@@ -1,14 +1,26 @@
 class Student
     attr_accessor :last_name, :first_name, :patronymic, :id, :phone, :telegram, :email, :git
-    def initialize(last_name, first_name, patronymic: nil, id: nil, phone: nil, telegram: nil, email: nil, git: nil)        @last_name = last_name
+    def initialize(last_name, first_name, patronymic: nil, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
+        @last_name = last_name
         @first_name = first_name
         @patronymic = patronymic
         @id = id
-        @phone = phone
+        self.phone = phone
         @telegram = telegram
         @email = email
         @git = git
     end
     def to_s
-    "ID: #{@id}, Last Name: #{@last_name}, First Name: #{@first_name}, Patronymic: #{@patronymic || 'not specified'}, Phone: #{@phone || 'not specified'}, Telegram: #{@telegram || 'not specified'}, E-Mail: #{@email || 'not specified'}, Git: #{@git || 'not specified'}"    end
+        "ID: #{@id}, Last Name: #{@last_name}, First Name: #{@first_name}, Patronymic: #{@patronymic || 'not specified'}, Phone: #{@phone || 'not specified'}, Telegram: #{@telegram || 'not specified'}, E-Mail: #{@email || 'not specified'}, Git: #{@git || 'not specified'}"
+    end
+    def phone=(phone)
+        if phone.nil? || Student.phone_regex_valid?(phone)
+            @phone = phone
+        else
+            raise ArgumentError, "Incorrect phone format: #{phone}"
+        end
+    end
+    def self.phone_regex_valid?(phone)
+        phone.match?(/^\+?[1-9][0-9]{7,14}$/)
+    end
 end
