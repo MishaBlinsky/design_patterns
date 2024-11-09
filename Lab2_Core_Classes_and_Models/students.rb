3d83a3c47fa6ab51d770058712f5b77aa0669602
Misha Blinsky <99417065+MishaBlinsky@users.noreply.github.com>
2024-11-09 10:06:13 +0300

    Lab 2 Task 1.4 - added attributes to class

--- Lab2_Core_Classes_and_Models/students.rb
+++ Lab2_Core_Classes_and_Models/students.rb
@@ -1,4 +1,5 @@
 class Student
+    attr_accessor :last_name, :first_name, :patronymic, :id, :phone, :telegram, :email, :git
     def initialize(last_name, first_name, patronymic, id = nil, phone = nil, telegram = nil, email = nil, git = nil)
         @last_name = last_name
         @first_name = first_name
@@ -11,53 +12,5 @@ class Student
     end
     def to_s
     "ID: #{@id}, Last Name: #{@last_name}, First Name: #{@first_name}, Patronymic: #{@patronymic}, Phone: #{@phone}, Telegram: #{@telegram}, E-Mail: #{@email}, Git: #{@git}"
-    end
-    def get_last_name
-        @last_name
-    end
-    def set_last_name (last_name)
-        @last_name = last_name
-    end
-    def get_first_name
-        @first_name
-    end
-    def set_first_name (first_name)
-        @first_name = first_name
-    end
-    def get_patronimyc
-        @patronimyc
-    end
-    def set_patronimyc (patronimyc)
-        @patronimyc = patronimyc
-    end
-    def get_id
-        @id
-    end
-    def set_id (id)
-        @id = id
-    end
-    def get_phone
-        @phone
-    end
-    def set_phone (phone)
-        @phone = phone
-    end
-    def get_telegram
-        @telegram
-    end
-    def set_telegram (telegram)
-        @telegram = telegram
-    end
-    def get_email
-        @email
-    end
-    def set_email (email)
-        @email = email
-    end
-    def get_git
-        @git
-    end
-    def set_git (git)
-        @git = git
     end
 end
