658c38ebacd0303034e6b9350d601de4ac0537fc
Misha Blinsky <99417065+MishaBlinsky@users.noreply.github.com>
2024-11-16 10:34:54 +0300

    Lab 2 Task 1.8 - added regex to other fields

--- Lab2_Core_Classes_and_Models/students.rb
+++ Lab2_Core_Classes_and_Models/students.rb
@@ -1,26 +1,75 @@
 class Student
     attr_accessor :last_name, :first_name, :patronymic, :id, :phone, :telegram, :email, :git
     def initialize(last_name, first_name, patronymic: nil, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
-        @last_name = last_name
-        @first_name = first_name
-        @patronymic = patronymic
-        @id = id
+        self.last_name = last_name
+        self.first_name = first_name
+        self.patronymic = patronymic
+        self.id = id
         self.phone = phone
-        @telegram = telegram
-        @email = email
-        @git = git
+        self.telegram = telegram
+        self.email = email
+        self.git = git
     end
     def to_s
         "ID: #{@id}, Last Name: #{@last_name}, First Name: #{@first_name}, Patronymic: #{@patronymic || 'not specified'}, Phone: #{@phone || 'not specified'}, Telegram: #{@telegram || 'not specified'}, E-Mail: #{@email || 'not specified'}, Git: #{@git || 'not specified'}"
     end
+    def last_name=(last_name)
+        if Student.name_regex_valid?(last_name)
+            @last_name = last_name
+        else
+            raise ArgumentError, "Only letters in names: #{last_name}"
+        end
+    end
+    def first_name=(first_name)
+        if Student.name_regex_valid?(first_name)
+            @first_name = first_name
+        else
+            raise ArgumentError, "Only letters in names: #{first_name}"
+        end
+    end
+    def patronymic=(patronymic)
+        if patronymic.nil? || Student.name_regex_valid?(patronymic)
+            @patronymic = patronymic
+        else
+            raise ArgumentError, "Only letters in names: #{patronymic}"
+        end
+    end
+    def id=(id)
+        if id.is_a?(Integer)
+            @id = id
+        else
+            raise ArgumentError, "ID must be a number: #{id}"
+        end
+    end
     def phone=(phone)
-        if phone.nil? || Student.phone_regex_valid?(phone)
+        if phone.nil? || phone.match?(/^\+?[1-9][0-9]{7,14}$/)
             @phone = phone
         else
             raise ArgumentError, "Incorrect phone format: #{phone}"
         end
     end
-    def self.phone_regex_valid?(phone)
-        phone.match?(/^\+?[1-9][0-9]{7,14}$/)
+    def telegram=(telegram)
+        if telegram.nil? || telegram.match?(/\A@[\w\d_]+\z/)
+            @telegram = telegram
+        else
+            raise ArgumentError, "Incorrect telegram format: #{telegram}"
+        end
+    end
+    def email=(email)
+        if email.nil? || email.match?(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+            @email = email
+        else
+            raise ArgumentError, "Incorrect e-mail format: #{email}"
+        end
+    end
+    def git=(git)
+        if git.nil? || git.match?(/^[a-zA-Z0-9-]{0,38}$/)
+            @git = git
+        else
+            raise ArgumentError, "Incorrect git format: #{git}"
+        end
+    end
+    def self.name_regex_valid?(name)
+        name.match?(/\A[А-Яа-яЁёA-Za-z]+\z/)
     end
 end
