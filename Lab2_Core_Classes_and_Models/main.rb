2aaac35a0d5b0266c94479136fc5c28b5905f159
Misha Blinsky <99417065+MishaBlinsky@users.noreply.github.com>
2024-11-16 11:55:54 +0300

    Lab 2 Task 1.10 - made use of set_contacts method

--- Lab2_Core_Classes_and_Models/main.rb
+++ Lab2_Core_Classes_and_Models/main.rb
@@ -2,8 +2,13 @@ require_relative 'students'
 begin
     student1 = Student.new("Morales", "Miles", id: 1, phone: "+76665554433", git: "abas-git")
     puts student1
-    student2 = Student.new("Zubenko", "Mihail", patronymic: "Petrovich", id: 2, phone: "1234567", git: "mafia-git")
+    student2 = Student.new("Zubenko", "Mihail", patronymic: "Petrovich", id: 2, telegram: "@mafioznik", git: "mafia-git")
     puts student2
+    student3 = Student.new("Stone", "Karen", id: 3, email: "ceoofsjw@notok.com", git: "woke-git")
+    puts student3
+    student1.set_contacts(phone: "+34445556677", telegram: "@ourspider")
+    puts "Updated student info:"
+    puts student1
 rescue ArgumentError => e
     puts e.message
 end
